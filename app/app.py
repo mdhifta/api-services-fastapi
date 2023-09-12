08cf4a22befab94e09330f97b0e9dc774fc9eec4
Mohamad Dhifta Ramadhan <Mohamad.Ramadhan@DDC.LOCAL>
2023-09-12 10:36:48 +0700

    add readme

--- app/app.py
+++ app/app.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from fastapi.openapi.docs import get_swagger_ui_html
-from fastapi.staticfiles import StaticFiles
+from fastapi.openapi.utils import get_openapi
 
+# add flow route from api
 from .api import services
 
-app = FastAPI(title='FastFest Template', summary='Template FastAPI by mdhifta', description="build with python 3.8.10")
+app = FastAPI()
 
 # base url access
 @app.get("/", tags=['Index'])
@@ -13,3 +14,17 @@ async def root() -> dict:
 
 # if you want add new router class, just copy and edit tags name
 app.include_router(services.router, tags=["Services"], prefix="/api")
+
+
+# swagger modification, change if you want
+def swagger():
+    app.openapi_schema = get_openapi(
+        title="FastFest Template",
+        version="FastAPI",
+        description="Build with python 3.8.10 by mdhifta",
+        routes=app.routes,
+    )
+    
+    return app.openapi_schema
+
+app.openapi = swagger
